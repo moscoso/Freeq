eb06e45ce5be4bfff71820133f86a04133d7d936
Brandon <bgarcia7@stanford.edu>
2015-12-20 02:37:42 -0500

    integrated yt server

--- server.py
+++ server.py
@@ -6,12 +6,19 @@ from flask import abort
 from flask import make_response
 from flask import redirect
 
+from yt_client import yt_client
+
 import sys
 import os
+import json
 
 #=====[ Sets up directories  ]=====
 base_dir = os.path.split(os.path.realpath(__file__))[0]
 static_dir = os.path.join(base_dir, 'static')
+
+#=====[ Instantiate our youtube client  ]=====
+yt = yt_client()
+MAX_RESULTS = 20
 
 #=====[ Initializes app  ]=====
 app = Flask(__name__, static_folder=static_dir)
@@ -33,6 +40,10 @@ def room():
 def search():
 	return render_template("search.html.jinja2")
 
+@app.route("/search_yt/<query>")
+def search(query):
+	results = yt.search(query, MAX_RESULTS)
+	return json.dumps(results)
 
 if __name__ == "__main__":
 	app.run()
